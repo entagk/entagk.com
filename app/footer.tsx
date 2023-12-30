edcc5c0db7227f0e35dd7a891dfb2adf6aa65d29
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-12-30 22:14:48 +0200

    style: make footer in responsive design

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,18 +3,14 @@ import React from "react";
 
 function Footer() {
 	return (
-		<footer className="w-full text-center text-[#e7e7e7] p-4 absolute bottom-0 right-0">
-			<div className="row">
-				Copyright © 2024 Entagk.com. All rights reserved
-			</div>
-			<div className="row">
-				<Link
-					href="#"
-					target="_blank"
-					className="capitalize hover:text-white">
-					report feature/issue
-				</Link>
-			</div>
+		<footer className="w-full text-center text-[#e7e7e7] py-8 min-[320px]:text-sm min-[320px]:py-9 min-[375px]:px-8 absolute bottom-0 right-0 sm:text-base">
+			<p className="min-[320px]:px-2">Copyright © 2024 Entagk.com. All rights reserved</p>
+			<Link
+				href="#"
+				target="_blank"
+				className="capitalize hover:text-white">
+				report feature/issue
+			</Link>
 		</footer>
 	);
 }
