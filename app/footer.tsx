92ef159262e86ccccdad96d63b0f8e8f17b5df81
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-12-30 23:06:37 +0200

    fix: add report issue/feature link

--- app/footer.tsx
+++ app/footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
 		<footer className="w-full text-center text-[#e7e7e7] py-8 min-[320px]:text-sm min-[320px]:py-9 min-[375px]:px-8 absolute bottom-0 right-0 sm:text-base">
 			<p className="min-[320px]:px-2">Copyright © 2024 Entagk.com. All rights reserved</p>
 			<Link
-				href="#"
+				href="https://forms.gle/uZmCpPJaXfukHx237"
 				target="_blank"
 				className="capitalize hover:text-white">
 				report feature/issue
